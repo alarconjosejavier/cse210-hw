83d831f6a87322a8d89b39d940f21afaddbdb470
alarconjosejavier <alarconjosejavier@hotmail.com>
2025-09-06 12:04:08 -0600

    Exercise 4 done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> userList = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        Console.Write("Enter number: ");
+        int userInput = int.Parse(Console.ReadLine()); ;
+
+        while (userInput != 0)
+        {
+            Console.Write("Enter number: ");
+            if (userInput != 0)
+            {
+                userList.Add(userInput);
+            }
+            userInput = int.Parse(Console.ReadLine());
+        }
+      
+        Console.WriteLine($"The sum is: {userList.Sum()}");
+        Console.WriteLine($"The average is: {userList.Average()}");
+        Console.WriteLine($"The largest number is: {userList.Max()}");
     }
 }
