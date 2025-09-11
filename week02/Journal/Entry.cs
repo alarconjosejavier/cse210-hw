39c4fb3a33b9d1f32aef10b66f6b5233582facdf
alarconjosejavier <alarconjosejavier@hotmail.com>
2025-09-11 09:41:40 -0600

    stubs completed

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,13 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class Entry
+{
+    public string _date;
+    public string _promptMessage;
+    public string _entryMessage;
+
+    public void Display()
+    {
+
+    }
+}
---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,25 @@
+public class Journal
+{
+    public List<Journal> _journals;
+
+    public void AddJournal(Journal newJournal)
+    {
+
+    }
+
+    public void DisplayJournals()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadToFile(string file)
+    {
+
+    }
+}
+
