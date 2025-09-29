1e7ff4d163604cd972e13ace2d3174479b6577e3
Jose Alarcon <macbookjosejavier@Joses-MacBook-Air.local>
2025-09-29 11:39:12 -0600

    Learning activity done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment first = new Assignment("Samuel Bennett","Multiplication");
+        Console.WriteLine(first.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment first1 = new MathAssignment("Joselito","Fraction","7.3","8-19");
+        Console.WriteLine(first1.GetSummary());
+        Console.WriteLine(first1.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WrittingAssignment first2 = new WrittingAssignment("Mary Waters","European History","The Causes of World War II");
+        Console.WriteLine(first2.GetSummary());
+        Console.WriteLine(first2.GetWrittingInformation());
     }
 }
